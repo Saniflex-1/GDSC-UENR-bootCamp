850da395c1b7f5d2fb4d183e07ae77110dc9ede5
Sani_flex <80122168+Saniflex-1@users.noreply.github.com>
2024-03-19 01:57:34 +0000

    Update index.js

--- styles/index.js
+++ styles/index.js
@@ -6,4 +6,24 @@ burGer.addEventListener("click", ()=>{
     // document.body.style.backgroundColor= 'olive';
 
     allNav.classList.toggle('header-active');
-})
+});
+
+
+let allLink = document.querySelector(".all-link");
+let appLink = document.querySelector(".app-link");
+let webLink = document.querySelector(".web-link");
+let cardLink = document.querySelector(".card-link");
+let popIn = document.querySelector(".pop");
+
+
+let All = document.getElementById("Alls");
+let App = document.querySelector(".app");
+let Web = document.getElementById("Web");
+let Card = document.getElementById("Card");
+
+
+appLink.addEventListener("click", ()=>{
+    Web.style.display="none";
+    Card.style.display="none";
+    document.body.style.backgroundColor="olive"; 
+});
