d223ec20178d0cc4366303c0d2873d5d7c8c14aa
Sani_flex <80122168+Saniflex-1@users.noreply.github.com>
2024-05-23 00:43:15 +0100

    okay

--- styles/index.js
+++ styles/index.js
@@ -1,13 +1,11 @@
 let burGer = document.querySelector(".humber");
 let allNav = document.querySelector(".header");
 
+burGer.addEventListener("click", () => {
+  // document.body.style.backgroundColor= 'olive';
 
-burGer.addEventListener("click", ()=>{
-    // document.body.style.backgroundColor= 'olive';
-
-    allNav.classList.toggle('header-active');
+  allNav.classList.toggle("header-active");
 });
-
 
 let allLink = document.querySelector(".all-link");
 let appLink = document.querySelector(".app-link");
@@ -15,15 +13,25 @@ let webLink = document.querySelector(".web-link");
 let cardLink = document.querySelector(".card-link");
 let popIn = document.querySelector(".pop");
 
-
 let All = document.getElementById("Alls");
 let App = document.querySelector(".app");
 let Web = document.getElementById("Web");
 let Card = document.getElementById("Card");
 
-
-appLink.addEventListener("click", ()=>{
-    Web.style.display="none";
-    Card.style.display="none";
-    document.body.style.backgroundColor="olive"; 
+appLink.addEventListener("click", () => {
+  Web.style.display = "none";
+  Card.style.display = "none";
+  document.body.style.backgroundColor = "olive";
 });
+
+const arrowUp = document.querySelector(".arrowjet");
+
+function showArrowTop() {
+  if (this.scrollY > this.innerHeight / 4) {
+    arrowUp.classList.add("showjet");
+  } else {
+    arrowUp.classList.remove("showjet");
+  }
+}
+
+window.addEventListener("scroll", showArrowTop);
